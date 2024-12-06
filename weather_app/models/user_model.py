e892bcdff860dd7e7683103a23ab59539aa4e774
Jenny Yang <jennyang@bu.edu>
2024-12-06 17:17:49 -0500

    fix import in user model

--- weather_app/models/user_model.py
+++ weather_app/models/user_model.py
@@ -4,8 +4,8 @@ import os
 
 from sqlalchemy.exc import IntegrityError
 
-from meal_max.db import db
-from meal_max.utils.logger import configure_logger
+from weather_app.utils.db import db
+from weather_app.utils.logger import configure_logger
 
 
 logger = logging.getLogger(__name__)
