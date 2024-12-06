f7140da2967ad69afe54ef565040a61c95808497
Jenny Yang <jennyang@bu.edu>
2024-12-06 16:25:11 -0500

    db.py file

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, Response, request
 from dotenv import load_dotenv
+from werkzeug.exceptions import BadRequest, Unauthorized
+
+
+from weather_app.models.favorites_model import FavoritesModel
+from weather_app.models.location_model import Location
+from weather_app.utils import db
 import os
 
 # Load environment variables from .env
@@ -14,6 +20,23 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 # Access API settings
 API_URL = os.getenv('API_URL')
 API_KEY = os.getenv('API_KEY')
+
+
+def create_app(config_class=ProductionConfig):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)  # Initialize db with app
+    with app.app_context():
+        db.create_all()  # Recreate all tables
+
+    favorites_model = FavoritesModel()
+
+    ####################################################
+    #
+    # Healthchecks
+    #
+    ####################################################
 
 @app.route('/')
 def home():
